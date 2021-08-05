87da8d16d0994fbeca6700ad34fd65e4f14f49b5
Piotr Gustab <pgustab@aol.com>
2021-08-04 22:17:58 -0400

    Add react-redux

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -6,6 +6,10 @@ export const settings = {
     buttonCancel: 'Cancel',
     defaultText: 'Add new item',
   },
+  search: {
+    defaultText: 'Search...',
+    icon: 'search',
+  },
   defaultListDescription: '<p>I can do all the things!!!</p>',
   defaultColumnIcon: 'list-alt',
 };
@@ -27,11 +31,11 @@ export const listData = {
       cards: [
         {
           key: 0,
-          title: 'The Witcher',
+          title: 'This Is Going to Hurt',
         },
         {
           key: 1,
-          title: 'Rich Dad, Poor Dad',
+          title: 'Interpreter of Maladies',
         },
       ],
     },
@@ -42,11 +46,11 @@ export const listData = {
       cards: [
         {
           key: 0,
-          title: 'The Terminal',
+          title: 'Harry Potter',
         },
         {
           key: 1,
-          title: 'Home Alone',
+          title: 'Star Wars',
         },
       ],
     },
@@ -57,13 +61,91 @@ export const listData = {
       cards: [
         {
           key: 0,
-          title: 'GTA V',
+          title: 'The Witcher',
         },
         {
           key: 1,
-          title: 'Need For Speed',
+          title: 'Skyrim',
         },
       ],
     },
   ],
 };
+
+const lists = [
+  {
+    id: 'list-1',
+    title: 'Things to do <sup>soon!</sup>',
+    description: 'Interesting things I want to check out!',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
+];
+
+const columns = [
+  {
+    id: 'column-1',
+    listId: 'list-1',
+    title: 'Books',
+    icon: 'book',
+  },
+  {
+    id: 'column-2',
+    listId: 'list-1',
+    title: 'Movies',
+    icon: 'film',
+  },
+  {
+    id: 'column-3',
+    listId: 'list-1',
+    title: 'Games',
+    icon: 'gamepad',
+  },
+  {
+    id: 'column-4',
+    listId: 'list-2',
+    title: 'Strange column',
+    icon: 'question',
+  },
+];
+
+const cards = [
+  {
+    id: 'card-1',
+    columnId: 'column-1',
+    title: 'This Is Going to Hurt',
+  },
+  {
+    id: 'card-2',
+    columnId: 'column-1',
+    title: 'Interpreter of Maladies',
+  },
+  {
+    id: 'card-3',
+    columnId: 'column-2',
+    title: 'Harry Potter',
+  },
+  {
+    id: 'card-4',
+    columnId: 'column-2',
+    title: 'Star Wars',
+  },
+  {
+    id: 'card-5',
+    columnId: 'column-3',
+    title: 'The Witcher',
+  },
+  {
+    id: 'card-6',
+    columnId: 'column-3',
+    title: 'Skyrim',
+  },
+];
+
+const initialStoreData = {
+  app: {...pageContents},
+  lists: [...lists],
+  columns: [...columns],
+  cards: [...cards],
+};
+
+export default initialStoreData;
