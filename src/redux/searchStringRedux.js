afad3768242dc843f7d148a536635e7fd48e1b76
Piotr Gustab <pgustab@aol.com>
2021-08-08 23:08:03 -0400

    Add search button

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,16 +1,24 @@
 // selectors
+export const getSearchString = ({searchString}) => searchString;
+export const countAllCards = ({cards}) => cards.length;
+export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 
 // action name creator
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const CHANGE = createActionName('CHANGE');
 
 // action creators
+export const createActionChange = payload => ({payload, type: CHANGE });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return action.payload;
     default:
       return statePart;
   }
 }
-
